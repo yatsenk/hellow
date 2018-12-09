1558781baea707652eb70767ff377b6e7753ff5d
NYatsenko <noyatsenko@ya.ru>
2018-12-09 17:01:34 +0300

    Revert "Java Headfirst"
    This reverts commit a6abcbe6d8c4ae9cea1732bed5be0b9480a5356c.

--- java/HeadFirstJava-master/EchoTestDrive/Echo.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Taly on 21.02.2017.
- */
-public class Echo {
-    int count = 0;
-    void hello() {
-        System.out.println("привеееееет...");
-    }
-}
--- java/HeadFirstJava-master/README.md
+++
@@ -1,3 +0,0 @@
-# Head First Java
-Краткий конспект и Исходные коды задач из книги Head First Java
-
